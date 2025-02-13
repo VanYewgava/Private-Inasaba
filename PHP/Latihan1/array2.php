7548b4646c77718fd14c7f4a6601585e8270ea21
Ivan Yugava <117693988+VanYewgava@users.noreply.github.com>
2025-02-13 22:33:33 +0700

    Delete PHP/Latihan1 directory

--- PHP/Latihan1/array2.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$buah = ["apel","jeruk","mangga","durian","alpukat","nanas","semangka","buah naga","pepaya"];
-for ($i=0; $i < count($buah) ; $i++) { 
-    echo $buah[$i] . "<br>";
-}
-
-?>
--- PHP/Latihan1/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-// perintah cetak di browser
-// echo
-// print
-// print_r()
-// var_dump()
-
-var_dump( "Belajar coding php dasar");
-?>
